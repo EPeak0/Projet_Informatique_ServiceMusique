141c61f4ba5c78e6d4ff3f5c770a4c50b118a04e
diemo <die.more@outlook.com>
2023-05-08 15:02:50 +0200

    First Push

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.projet_informatique_servicemusique {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.projet_informatique_servicemusique to javafx.fxml;
+    exports com.example.projet_informatique_servicemusique;
+}
