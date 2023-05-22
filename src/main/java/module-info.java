d62b13d30438179fbc5580cbc83cb6f35188093f
theot <theo.tissieres@students.hevs.ch>
2023-05-22 15:44:24 +0200

    Fonction du bouton PLAY

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.projet_informatique_servicemusique {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.projet_informatique_servicemusique to javafx.fxml;
